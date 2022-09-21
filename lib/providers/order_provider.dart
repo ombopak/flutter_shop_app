f5a5cac55e4439068e650f2f3a65bec7f97506ba
msyndev <msyndev@ombopak.com>
2022-09-21 12:48:46 +0700

    continue

--- lib/providers/order_provider.dart
+++ lib/providers/order_provider.dart
@@ -21,13 +21,54 @@ class OrderProvider with ChangeNotifier {
     return [..._orders];
   }
 
-  void addOrder(List<CartItem> cartProducts, double total) {
+  Future<void> fetchAndSetOrders() async {
+    final url = Uri.parse(
+        'https://shop-app-c5564-default-rtdb.asia-southeast1.firebasedatabase.app/orders.json');
+    final response = await http.get(url);
+    final List<OrderItem> loadedOrders = [];
+    final extractedData = json.decode(response.body) as Map<String, dynamic>;
+    extractedData.forEach((orderId, orderData) {
+      loadedOrders.add(OrderItem(
+          id: orderId,
+          amount: orderData['amount'],
+          dateTime: DateTime.parse(
+            orderData['dateTime'],
+          ),
+          products: (orderData['products'] as List<dynamic>).map((item) =>
+              CartItem(
+                  id: item['id'],
+                  price: item['price'],
+                  quantity: item['quantity'],
+                  title: item['item'],
+                  )
+                  )));
+    });
+  }
+
+  Future<void> addOrder(List<CartItem> cartProducts, double total) async {
+    final url = Uri.parse(
+        'https://shop-app-c5564-default-rtdb.asia-southeast1.firebasedatabase.app/orders.json');
+    final timestamp = DateTime.now();
+    final response = await http.post(url,
+        body: json.encode({
+          'amount': total,
+          'dateTime': timestamp.toIso8601String(),
+          'product': cartProducts
+              .map((cp) => {
+                    'id': cp.id,
+                    'title': cp.title,
+                    'quantity': cp.quantity,
+                    'price': cp.price,
+                  })
+              .toList(),
+        }));
+
     _orders.insert(
         0,
         OrderItem(
-            id: DateTime.now().toString(),
+            id: json.decode(response.body)['name'],
             amount: total,
-            dateTime: DateTime.now(),
+            dateTime: timestamp,
             products: cartProducts));
     notifyListeners();
   }
